2aadc9a59cf004b0b11a8ab0b0fd9e6a37c0579c
Atakan Sakaoğlu <68845949+sakaoglua@users.noreply.github.com>
2023-03-28 14:53:15 +0300

    Add files via upload
    Files added!

---
+++ DaireAlanCevreHesaplama/DaireAlanCevreHesaplama/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DaireAlanCevreHesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_yariCap = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbx_yariCap = new System.Windows.Forms.TextBox();
+            this.btn_hesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_yariCap
+            // 
+            this.lbl_yariCap.AutoSize = true;
+            this.lbl_yariCap.BackColor = System.Drawing.Color.Red;
+            this.lbl_yariCap.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_yariCap.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_yariCap.Location = new System.Drawing.Point(63, 38);
+            this.lbl_yariCap.Name = "lbl_yariCap";
+            this.lbl_yariCap.Size = new System.Drawing.Size(159, 39);
+            this.lbl_yariCap.TabIndex = 0;
+            this.lbl_yariCap.Text = "Yarıçap: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Fuchsia;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(59, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Alan:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Fuchsia;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(59, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Çevre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(247, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 39);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(247, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 39);
+            this.label5.TabIndex = 4;
+            // 
+            // tbx_yariCap
+            // 
+            this.tbx_yariCap.Location = new System.Drawing.Point(254, 53);
+            this.tbx_yariCap.Name = "tbx_yariCap";
+            this.tbx_yariCap.Size = new System.Drawing.Size(201, 22);
+            this.tbx_yariCap.TabIndex = 5;
+            // 
+            // btn_hesapla
+            // 
+            this.btn_hesapla.BackColor = System.Drawing.Color.OrangeRed;
+            this.btn_hesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_hesapla.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_hesapla.Location = new System.Drawing.Point(66, 314);
+            this.btn_hesapla.Name = "btn_hesapla";
+            this.btn_hesapla.Size = new System.Drawing.Size(181, 52);
+            this.btn_hesapla.TabIndex = 6;
+            this.btn_hesapla.Text = "Hesapla";
+            this.btn_hesapla.UseVisualStyleBackColor = false;
+            this.btn_hesapla.Click += new System.EventHandler(this.btn_hesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlueViolet;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_hesapla);
+            this.Controls.Add(this.tbx_yariCap);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_yariCap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_yariCap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbx_yariCap;
+        private System.Windows.Forms.Button btn_hesapla;
+    }
+}
+
---
+++ DaireAlanCevreHesaplama/DaireAlanCevreHesaplama/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DaireAlanCevreHesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const double pisayisi = 3.14;
+        int yaricap;
+        double alansonuc;
+        double cevresonuc;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_hesapla_Click(object sender, EventArgs e)
+        {
+            yaricap = Convert.ToInt32(tbx_yariCap.Text);
+            alansonuc = pisayisi * Math.Pow(yaricap,2);
+            label4.Text = Convert.ToString(alansonuc);
+            cevresonuc = 2 * pisayisi * yaricap;
+            label5.Text = Convert.ToString(cevresonuc);
+        }
+    }
+}
